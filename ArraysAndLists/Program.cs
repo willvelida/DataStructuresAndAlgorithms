c5ead3729f2d0861828554a63e87eb484d165552
Will Velida <willvelida@hotmail.co.uk>
2019-04-20 15:28:27 +1200

    Adding list work to project

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -33,6 +33,38 @@ namespace ArraysAndLists
             BubbleSort.Sort(integerValues);
             Console.WriteLine("Bubble Sort results");
             Console.WriteLine(string.Join(" | ", integerValues));
+
+            // List of people
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Will", Country = CountryEnum.UK, Age = 28 });
+            people.Add(new Person() { Name = "James", Country = CountryEnum.PL, Age = 19 });
+            people.Add(new Person() { Name = "Clair", Country = CountryEnum.DE, Age = 48 });
+
+            // Sort the list by names of poeple in ascending order
+            List<Person> results = people.OrderBy(p => p.Name).ToList();
+
+            // Iterate through the list
+            foreach (Person person in results)
+            {
+                Console.WriteLine($"{person.Name} ({person.Age} years) from {person.Country}.");
+            }
+
+            
+
+            List<double> numbers = new List<double>();
+            do
+            {
+                Console.WriteLine("Enter a number!");
+                string numberString = Console.ReadLine();
+                if (!double.TryParse(numberString, NumberStyles.Float, new NumberFormatInfo(), out double number))
+                {
+                    break;
+                }
+                numbers.Add(number);
+                Console.WriteLine($"The average value: {numbers.Average()}");
+            } while (true);
+
+            
         }
     }
 }
